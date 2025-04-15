46d415ce058b68f42ab100b970158d5f23b30fd5
Hadrien MENS-PELLEN <hadrien_mens-pellen@ext.connect-tech.sncf>
2025-04-15 10:48:23 +0200

    fix: correct command output redirection and display available sandboxes

--- bootstrap-kata.sh
+++ bootstrap-kata.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 check_dependencies() {
     local dependencies=("nix" "git" "direnv")
     for dep in "${dependencies[@]}"; do
-        if ! command -v "$dep" &> /dev/null; then
+        if ! command -v "$dep" &>/dev/null; then
             echo "Error: $dep is not installed." >&2
             exit 1
         fi
@@ -21,6 +21,7 @@ list_sandboxes() {
     fi
 
     echo "Available Sandboxes:"
+    echo $output
     echo "$output" | grep -E '^[[:space:]]+\w+/' | sed 's/^[[:space:]]*//'
 }
 
